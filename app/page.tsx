e7be5c31731e9b2b79e765d7382c85a5e7b8cb99
Dawood Faisal <dawood_faisal@yahoo.com>
2024-10-10 03:33:52 +0500

    commit-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Intro from '@/components/intro'
 import React from 'react'
 
 export default function Home() {
   return (
     <section className="py-24">
-      <div className="container">
-        <h1 className="text-3xl font-bold">Portfolio</h1>
+      <div className="container max-w-3xl">
+        <Intro/>
       </div>
     </section>
   )
