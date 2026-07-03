0f87e569a8e57367d915fec44ca04ca259f0f483
Dawood Faisal <dawood_faisal@yahoo.com>
2026-07-03 20:17:22 +0500

    fixed error for production

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Intro from '@/components/intro'
 import NewsletterForm from '@/components/newsletter-form'
 import RecentPosts from '@/components/recent-posts'
 import RecentProjects from '@/components/recent-projects'
-import { Metadata } from 'next'
 
 
 // export const metadata: Metadata = {
